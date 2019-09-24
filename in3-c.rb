08f8553016efcfc012af0bb733f1d79118c85cdf
pari <parithosh@slock.it>
2019-09-24 14:16:15 +0200

    initial commit

---
+++ in3-c.rb
@@ -0,0 +1,13 @@
+class In3C < Formula
+  desc "C-version of the in3 client "
+  homepage ""
+  url "https://github.com/slockit/in3-c/releases/download/v1.0.0/in3-1.0.0-mac.zip"
+  sha256 "efe56616fb82a2660ddc928ce2451033f1483bff51f7bcc76eaf4b18df0be0cb"
+
+
+  def install
+	bin.install "bin/in3"
+	bin.install "bin/rlp"
+  end
+
+end
