b70a60947f833b0eacdc19178817384972f84ff5
ga83buh <parithosh.jayanthi@tum.de>
2019-10-04 09:17:17 +0200

    updated version and hash

--- in3.rb
+++ in3.rb
@@ -1,8 +1,8 @@
 class In3 < Formula
   desc "C-version of the in3 client "
   homepage ""
-url "https://github.com/slockit/in3-c/releases/download/${CI_COMMIT_TAG}/in3_${CI_COMMIT_TAG}_mac.tar.gz"
-sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+url "https://github.com/slockit/in3-c/releases/download/v2.0.4-RC1/in3_v2.0.4-RC1_mac.tar.gz"
+sha256 "10703258c3b6cec74017afadc1f8872f57f211186f2e6aa1adb387b1cf9dd4d1"
 
   def install
 	bin.install "bin/in3"
