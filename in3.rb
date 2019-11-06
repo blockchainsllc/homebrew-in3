3c5be91c910efbe235585cea25c1f0537d7000b5
Parithosh Jayanthi <parithosh.jayanthi@tum.de>
2019-11-06 12:16:06 +0530

    Update in3.rb

--- in3.rb
+++ in3.rb
@@ -2,7 +2,7 @@ class In3 < Formula
   desc "C-version of the in3 client "
   homepage ""
 url "https://github.com/slockit/in3-c/releases/download/v2.0.6-RC1/in3_v2.0.6-RC1_mac.tar.gz"
-sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+sha256 "bb1df912b6900830929ee70e4c86bf9bfae1539c2966e05715ba318152822b79"
 
   def install
 	bin.install "bin/in3"
