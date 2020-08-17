08c2ecd5b89920dd147e54990fbbad8e62c6ac95
devops-slockIt <devops@slock.it>
2020-08-17 20:51:59 +0000

    updated version and hash

--- in3.rb
+++ in3.rb
@@ -1,8 +1,8 @@
 class In3 < Formula
   desc "C-version of the in3 client "
   homepage ""
-url "https://github.com/slockit/in3-c/releases/download/v2.5.4/in3_v2.5.4_mac.tar.gz"
-sha256 "6db6296c88301c9b8c1f4a4cde050eb16e6d0b149683080e7a7bdfd96041cd34"
+url "https://github.com/slockit/in3-c/releases/download/v3.0.2/in3_v3.0.2_mac.tar.gz"
+sha256 "411f45aac55087a3d16d3c04be1afef8c808940ac703f5e44badf01e78cd5952"
 
   def install
 	bin.install "bin/in3"
